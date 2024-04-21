b4ccc4f62783b9f6d0ba41213f79da4d1dda9bfa
Muskanmahi23 <93921249+Muskanmahi23@users.noreply.github.com>
2024-04-21 20:13:50 +0530

    Employee Management System

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <footer className='footer'>
+        <span>All rights reserved 2024 by javahardik</span>
+    </footer>
+  )
+}
+
+export default FooterComponent
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <nav className='navbar navbar-dark bg-dark'>
+            <a href="https://www.google.com" className="navbar-brand">Employee Management System</a>
+        </nav>
+    </div>
+  )
+}
+
+export default HeaderComponent
